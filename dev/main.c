4314b9936aae55e77b7f7814c7157054e072ea78
weibin <xiaobinqt@163.com>
2022-01-12 14:03:30 +0800

    fishc

---
+++ dev/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("cccccc\n");
+    return 0;
+}
---
+++ fishc/chapter1/test1.c
@@ -0,0 +1,12 @@
+//
+// Created by weibin on 2022-01-12.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("hello world \n");
+    return 0;
+}
+
+
